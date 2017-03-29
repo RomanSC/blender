2442eab385242fbbefca26accb8ec376be3c6fee
RomanSC <r.s.col988@gmail.com>
2017-03-29 16:38:08 -0400

    Sync

--- scripting/coolplane/coolplane.py
+++ scripting/coolplane/coolplane.py
@@ -9,13 +9,31 @@ import bpy, bgl, blf
 
 from math import *
 from mathutils import *
+import random
+
+def cool_mats():
+    # Create a few materials
+    red = bpy.data.materials.new('Red')
+    red.diffuse_color = (1,0,0)
+
+    blue = bpy.data.materials.new('Blue')
+    blue.diffuse_color = (0,0,1)
+
+    yellow = bpy.data.materials.new('Yellow')
+    yellow.diffuse_color = (1,1,0)
 
 def make_coolplane(plane):
     # Subdivide and make shape
     plane.select = True
     bpy.ops.object.mode_set(mode="EDIT")
 
-    bpy.ops.mesh.subdivide(number_cuts=10, fractal=0.5, fractal_along_normal=0.25, seed=8)
+    # bpy.ops.mesh.subdivide(number_cuts=20,
+    #                        fractal=2,
+    #                        fractal_along_normal=0.25,
+    #                        seed=8)
+    bpy.ops.mesh.subdivide(number_cuts=12,
+                           fractal=2,
+                           fractal_along_normal=0.25)
 
     # TODO:
     # Use material and shaders
@@ -25,15 +43,19 @@ def make_coolplane(plane):
     # bpy.ops.object.material_slot_assign()
     # bpy.context.object.active_material.use_nodes = True
 
+    bpy.ops.uv.unwrap(method="ANGLE_BASED",
+                      margin=0.001)
 
 def create_plane():
     bpy.ops.mesh.primitive_plane_add()
 
-    xyz = {"x": "8", "y": "8", "z": "8"}
+    xyz = {"x": "10", "y": "10", "z": "10"}
     for key, val in xyz.items():
         # hack
         exec("bpy.data.objects[\"Plane\"].scale." + key + " = " + val)
 
+    ob = bpy.context.object
+    ob.name = 'Coolplane'
 
 def delete_cube_and_lamp():
     """ Source - Stack Exchange:
@@ -59,7 +81,7 @@ def cursor_and_cam():
     scene = bpy.data.scenes["Scene"]
 
     # Set camera location
-    xyz = {"x": "0", "y": "0", "z": "15"}
+    xyz = {"x": "0", "y": "0", "z": "18"}
     for key, val in xyz.items():
         # hack
         exec("scene.camera.location." + key + " = " + val)
@@ -77,7 +99,9 @@ def main():
     delete_cube_and_lamp()
     create_plane()
 
-    plane = bpy.data.objects["Plane"]
+    plane = bpy.data.objects["Coolplane"]
+
+    cool_mats()
 
     make_coolplane(plane)
 
